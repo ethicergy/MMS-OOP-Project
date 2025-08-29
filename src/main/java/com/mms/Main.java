1c420a81d7205a8f0877efcf2b0c97830288b517
Joel Lijo Mathew <joellijo4@gmail.com>
2025-08-29 21:30:40 +0530

    On God, commiting and pushing, hope it works

--- src/main/java/com/mms/Main.java
+++ src/main/java/com/mms/Main.java
@@ -3,33 +3,31 @@ package com.mms;
 import com.mms.db.DBManager;
 import com.mms.dao.UserDAO;
 import com.mms.models.User;
-import com.mms.UI.*; 
+import com.mms.UI.LoginFrame;   // explicitly import LoginFrame
+import javax.swing.SwingUtilities;
 
 public class Main {
-public static void main(String[] args){
-	
-	try(DBManager db = new DBManager()) {
-	System.out.println("Shi working till now");
-	
-	
-	} catch (Exception e) {
-	e.printStackTrace();
-	}
-	
-	UserDAO userDAO = new UserDAO();
+    public static void main(String[] args) {
+        // --- Database test ---
+        try (DBManager db = new DBManager()) {
+            System.out.println("DB connection working till now");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	User newUser = new User("Protagonist", "tenet@gmail.com", "tenet", "user");
-	boolean working = userDAO.createUser(newUser);
-	
-	if(working) {
-		System.out.println("Shi working till now");
-		
-		
-	}
-	else {
-		System.out.println("F Shi not working");
+        UserDAO userDAO = new UserDAO();
+        User newUser = new User("Protagonist", "tenet@gmail.com", "tenet", "user");
+        boolean working = userDAO.createUser(newUser);
 
-	}
-	
-	}
-	}
+        if (working) {
+            System.out.println("User creation working");
+        } else {
+            System.out.println("User creation failed");
+        }
+
+        // --- Launch Login UI ---
+        SwingUtilities.invokeLater(() -> {
+            new LoginFrame().setVisible(true);
+        });
+    }
+}
